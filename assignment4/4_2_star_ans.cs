5622a900db7cddb3c4869d6385878df03eb9e529
ku-nian <nian6132@naver.com>
2024-11-03 14:35:19 +0900

    complete assignment 4-2

--- assignment4/4_2_star_ans.cs
+++ assignment4/4_2_star_ans.cs
@@ -10,10 +10,47 @@ namespace star
             int radius = int.Parse(Console.ReadLine());
             int size = 2 * (radius + 1);
 
-            // ---------- TODO ----------
+
+            char[] star = new char[size*2];
+            for (int i = 0; i < 2*size; i++) {
+                star[i] = ' ';
+            }
 
 
-            // --------------------
+            bool flag = true;
+            for (int i = 0; i < size; i++) {
+                if (i == 2) {
+                    star[1] = '*';
+                    for (int j = 2; j < size - 1; j++) {
+                        star[j] = ' ';
+                    }
+                }
+
+                if (i == 1 || i == size-1) {
+                    star[1] = ' '; 
+                    for (int j = 2; j < size - 1; j++) {
+                        star[j] = '*';
+                    }
+                }
+
+                if (flag) {
+                    for (int j = 0; j < size; j++) {
+                        star[size + j] = ' ';
+                    }
+                    star[size + size/3] = '*'; // 4,6,8 / 3 -> 1,2,2 // 4,6,8 % 3 -> 1,0,2
+                    star[size + size/3*2 + ((size%3 == 2) ? 1 : 0)] = '*';
+                    flag = false;
+                }
+
+                if (i == size/3 || i == size/3*2 + ((size%3 == 2) ? 1 : 0)) {
+                    for (int j = 0; j < size; j++) {
+                        star[size + j] = '*';
+                    }
+                    flag = true;
+                }
+                Console.WriteLine(star);
+                
+            }
         }
 
         // calculate the distance between (x1, y1) and (x2, y2)
